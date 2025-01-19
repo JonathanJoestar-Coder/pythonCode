0dcb4cb75dc5c9d101f7c647773676f6913d3f02
JonathanJoestar-Coder <jonathan.tate@i3-corps.com>
2025-01-18 18:07:51 -0600

    Update docker-composeCreation.py

--- docker-composeCreation.py
+++ docker-composeCreation.py
@@ -10,15 +10,22 @@ class Menu(tk.Tk):
         self.VSCodeExtensionCheck = tk.IntVar(self)
         self.VSCodeID = tk.StringVar(self)
         self.VSCodeExtensionList = []
+        self.rootAccessCheck = tk.IntVar(self)
         self.createWidgets()
 
     def createWidgets(self):
         self.VSCodeLabelFrame = tk.LabelFrame(self,text='VS Code Extenions')
-        self.VSCodeLabelFrame.grid(row=0,column=0)
+        self.VSCodeLabelFrame.grid(sticky='w',row=0,column=0)
         self.VSCodeExtensionCheckButton = tk.Checkbutton(self.VSCodeLabelFrame,text='Include VS Code Extensions?',variable=self.VSCodeExtensionCheck,onvalue=1,offvalue=0,command=self.VSCodeExtensionSelect)
         self.VSCodeExtensionCheckButton.grid(row=0,column=0,sticky='w')
         self.devEnvironmentCreateButton = tk.Button(text='Create Yaml for Dev Environment!',command=self.confirmationCreation)
-        self.devEnvironmentCreateButton.grid(row=1,column=0)
+        self.devEnvironmentCreateButton.grid(row=2,column=0)
+        self.rootAccessLabelFrame = tk.LabelFrame(self,text='Root access')
+        self.rootAccessLabelFrame.grid(sticky='w',row=1,column=0)
+        self.rootAccessRadioButtonYes = tk.Radiobutton(self.rootAccessLabelFrame,variable=self.rootAccessCheck,text='Yes',value=1)
+        self.rootAccessRadioButtonYes.grid(row=0,column=0)
+        self.rootAccessRadioButtonNo = tk.Radiobutton(self.rootAccessLabelFrame,variable=self.rootAccessCheck,text='No',value=2)
+        self.rootAccessRadioButtonNo.grid(row=0,column=1)
     def VSCodeExtensionSelect(self):
         if self.VSCodeExtensionCheck.get() == 1:
             self.VSCodeIDEntry = tk.Entry(self.VSCodeLabelFrame,textvariable=self.VSCodeID)
@@ -35,11 +42,17 @@ class Menu(tk.Tk):
         self.VSCodeExtensionList.append('{}'.format(self.VSCodeID.get()))
         self.VSCodeIDEntry.delete(0,END)
     def confirmationCreation(self):
+        self.VSCodeIDEntry.delete(0,END)
         self.confirmationWindow = Toplevel(self)
         self.confirmationWindow.title("Confirmation of Creation")
         self.confirmationWindow.geometry("200x200")
-        self.confirmationLabel = tk.Label(self.confirmationWindow,text='{}'.format(self.VSCodeExtensionList))
-        self.confirmationLabel.grid(row=0,column=0)
+        if self.VSCodeExtensionCheck.get() == 1:
+            self.VSCodeExtensionConfirmationLabel = tk.Label(self.confirmationWindow,text='List of VS Code Extension:')
+            self.VSCodeExtensionConfirmationLabel.grid(sticky='w',row=1,column=0)
+            for i in range(len(self.VSCodeExtensionList)):
+                self.confirmationLabel = tk.Label(self.confirmationWindow,text='{}'.format(self.VSCodeExtensionList[i]))
+                self.confirmationLabel.grid(sticky='w',row=i+2,column=0)
+        
 if __name__=="__main__":
     app = Menu()
     app.mainloop()
